2fe31bfc4c34ebbd6c87405c00e7b7144ba8bb0b
Katia <katia.sfihi@gmail.com>
2021-04-07 15:45:55 +0100

    imtegration of react router

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+
+const NavBar = () => {
+return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <link to='/' className='navbar-logo'>
+                    TVRL
+                </link>
+            </div>
+
+
+        </nav>
+    </>
+)
+}
+ export default NavBar
